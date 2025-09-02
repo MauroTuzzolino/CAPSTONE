ec202a4d1e97d0c828ae0916a730ea4b720181b8
MauroTuzzolino <tuzzolinomauro.prog05@gmail.com>
2025-09-02 09:55:19 +0200

    Modifica delle autorizzazioni per le patch

--- back-end/src/main/java/maurotuzzolino/back_end/controllers/UserController.java
+++ back-end/src/main/java/maurotuzzolino/back_end/controllers/UserController.java
@@ -1,8 +1,10 @@
 package maurotuzzolino.back_end.controllers;
 
 import maurotuzzolino.back_end.entities.User;
+import maurotuzzolino.back_end.enums.Role;
 import maurotuzzolino.back_end.exceptions.BadRequestException;
 import maurotuzzolino.back_end.services.UserService;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -17,28 +19,32 @@ public class UserController {
         this.userService = userService;
     }
 
-    //Modifica completa con POST (Admin o il proprietario)
+    // Modifica completa (Admin o il proprietario)
     @PostMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN') or #id == principal.id")
     public User updateUserPost(@PathVariable Long id,
-                               @RequestBody User updatedUser,
-                               @AuthenticationPrincipal User currentUser) {
+                               @RequestBody User updatedUser) {
         if (!id.equals(updatedUser.getId())) {
             throw new BadRequestException("L'ID nel path non corrisponde a quello nel body");
         }
         return userService.updateUserFull(id, updatedUser);
     }
 
-    //Modifica parziale con PATCH (Admin o il proprietario)
+    // Modifica parziale (Admin o il proprietario)
     @PatchMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN') or #id == principal.id")
     public User updateUserPatch(@PathVariable Long id,
                                 @RequestBody User partialUpdate,
                                 @AuthenticationPrincipal User currentUser) {
+
+        if (!currentUser.getId().equals(id) && currentUser.getRole() != Role.ADMIN) {
+            throw new AccessDeniedException("Non puoi modificare questo utente");
+        }
+
         return userService.updateUserPartial(id, partialUpdate);
     }
 
-    //Elimina utente (solo ADMIN)
+    // Elimina utente (solo ADMIN)
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public String deleteUser(@PathVariable Long id) {
