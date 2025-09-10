dbc85d1a7db15da4f377e058c85b878fb10ca752
MauroTuzzolino <tuzzolinomauro.prog05@gmail.com>
2025-09-10 15:28:16 +0200

    Modifiche di stile al footer

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
-import { BsFacebook, BsInstagram, BsTwitter, BsHouse } from "react-icons/bs";
+import { BsFacebook, BsInstagram, BsTwitter } from "react-icons/bs";
+import "../css/Footer.css";
 
 const Footer = () => {
   return (
-    <footer className="custom-footer bg-dark text-light py-4 mt-auto">
+    <footer className="custom-footer py-4 mt-auto">
       <Container>
         <Row className="d-flex justify-content-evenly text-center text-lg-start">
           {/* Colonna 1: Info */}
           <Col lg={4} className="mb-3 mb-lg-0">
-            <h5 className="mb-3">BLACKHOLE</h5>
-            <p className="small">Your window to space. News, events, and everything related to the universe.</p>
+            <h5 className="mb-3 footer-title">BLACKHOLE</h5>
+            <p className="small footer-text">Your window to space. News, events, and everything related to the universe.</p>
           </Col>
 
           {/* Colonna 2: Social */}
           <Col lg={4}>
-            <h6 className="mb-3">Seguici</h6>
+            <h6 className="mb-3 footer-subtitle">Seguici</h6>
             <div className="d-flex justify-content-center justify-content-lg-start gap-3">
               <a href="https://facebook.com" target="_blank" rel="noreferrer" className="social-icon">
                 <BsFacebook />
@@ -31,11 +31,11 @@ const Footer = () => {
           </Col>
         </Row>
 
-        <hr className="my-3 border-light" />
+        <hr className="my-3 footer-divider" />
 
         <Row>
           <Col className="text-center">
-            <p className="mb-0 small">&copy; {new Date().getFullYear()} BLACKHOLE. All rights reserved.</p>
+            <p className="mb-0 small footer-text">&copy; {new Date().getFullYear()} BLACKHOLE. All rights reserved.</p>
           </Col>
         </Row>
       </Container>
